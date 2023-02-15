1d8f14d1481967d6bddb439bce90e44597046355
SimpleMg <117079680+SimpleMg@users.noreply.github.com>
2023-02-15 17:02:31 +0100

    Update scrap.py

--- scrap.py
+++ scrap.py
@@ -7,7 +7,7 @@ import json
 class ScrapData:
     def __init__(self):
         self.energie = ["dies", "ess", "elec"]
-        self.marque = ["PEUGEOT"]
+        self.marque = ["PEUGEOT", "TESLA"]
         self.km_max = 40000
         self.km_min = 1
         self.prix_min = 10000
@@ -15,9 +15,9 @@ class ScrapData:
         self.annee_min = 2000
         self.annee_max = 2023
 
-    def _url(self, marque, page, modele):
+    def _url(self, marque ,page, modele):
         url = "https://www.lacentrale.fr/listing?makesModelsCommercialNames={}%3A{}&mileageMax={}&mileageMin=1{}&page={}&priceMax={}&priceMin={}&yearMax={}&yearMin={}".format(
-            self.marque[0].upper(), modele, self.km_max, self.km_min, page, self.prix_max, self.prix_min, self.annee_max, self.annee_min)
+            self.marque[marque].upper(), modele, self.km_max, self.km_min, page, self.prix_max, self.prix_min, self.annee_max, self.annee_min)
         return url
 
     def _browse_page(self, url, n):
@@ -38,7 +38,7 @@ class ScrapData:
         return str(url)
 
     def _data_org(self, data:list[str])->list[str]:
-        org_data = {"marque":0, "modele":0, "année":0, "km":0, "mode":0, "energie":0}
+        org_data = {"marque":0, "motor":0, "année":0, "km":0, "mode":0, "energie":0}
         energie_org_data = ["Diesel", "Essence", "Électrique"]
         mode = ["Automatique" , "Manuelle"]
         for i in range(len(data)):
@@ -49,7 +49,7 @@ class ScrapData:
                     data[i] = int(data[i])
                     org_data["année"] = data[i]
                 except:
-                    org_data["modele"] = data[i]
+                    org_data["motor"] = data[i]
             elif "km" in data[i].split():
                 org_data["km"] = "".join(data[i].split())
             elif data[i] in energie_org_data:
@@ -58,7 +58,7 @@ class ScrapData:
                 org_data["mode"] = data[i]
             else:
                 org_data["energie"] = data[i].replace("É", "E")
-        list_org_data = [org_data["marque"],org_data["modele"],org_data["année"],org_data["km"],org_data["mode"],org_data["energie"]]
+        list_org_data = [org_data["marque"],org_data["motor"],org_data["année"],org_data["km"],org_data["mode"],org_data["energie"]]
         return list_org_data
 
     def _recup_element_json(self, element_recup):
@@ -81,7 +81,7 @@ class ScrapData:
         marque = 0
         cpt = 1
         while marque != len(self.marque):
-            modele = self._recup_element_json("PEUGEOT")
+            modele = self._recup_element_json(self.marque[marque])
             print("modele = ", modele)
             for i in range(len(modele)):
                 car_list_h3 = [1]
@@ -103,13 +103,10 @@ class ScrapData:
                     list_tuple_cara = []
                     element = 0
                     while element != len(car_list_div_cara):
-                        print("div cara: ", len(car_list_div_cara))
-                        print("element: ", element)
                         try:
                             list_tuple_cara.append([car_list_div_cara[0], car_list_div_cara[1],
                                                 car_list_div_cara[2], car_list_div_cara[3], car_list_div_cara[4]])
                         except:
-                            print("div cara: ", car_list_div_cara)
                             list_tuple_cara.append([car_list_div_cara[0], car_list_div_cara[1],
                                                 car_list_div_cara[2], car_list_div_cara[3]]) 
                         element += 4
@@ -117,8 +114,9 @@ class ScrapData:
                     while iter_car != len(car_list_h3):
                         data = [car_list_h3[iter_car], car_list_div_modele[iter_car], list_tuple_cara[iter_car][0], list_tuple_cara[iter_car][1], list_tuple_cara[iter_car][2], list_tuple_cara[iter_car][3]]
                         data = self._data_org(data)
-                        print("marque = {}, modele = {} ,  {}, {}, {}, {}".format(
-                           data[0], data[1], data[2], data[3], data[4], data[5]))
+                        marque_model = data[0].split()
+                        print("marque = {}, modele = {} ,  motor = {}, annee = {}, km = {}, mode {}, essence = {}".format(
+                           marque_model[0], " ".join(marque_model[1::]), data[1], data[2], data[3], data[4], data[5]))
                         with open("file.csv", "a") as file_descriptor:
                             csv_writer = csv.writer(file_descriptor)
                             csv_writer.writerow(['{}'.format(data[0]), '{}'.format(data[1]), '{}'.format(data[2]), '{}'.format(data[3]) ,'{}'.format(data[4]), '{}'.format(data[5])])
